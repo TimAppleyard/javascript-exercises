fd77df9c2fef5672a9f275e74cd8764a8d2c8281
TimAppleyard <appleyard.timothy@gmail.com>
2026-07-06 18:34:58 -0500

    Completed the excersise for palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    formatted = input.replaceAll(",", "");
+    formatted = formatted.replaceAll(" ", "");
+    formatted = formatted.replaceAll("!", "");
+    formatted = formatted.replaceAll(".", "");
+    formatted = formatted.toLowerCase();
+    reversedFormatted = formatted.split("").reverse().join("");
+    console.log("HERE IS THE INPUT: " + input);
+    console.log("HERE IS THE FORMATTED STRING: " + reversedFormatted)
+    return reversedFormatted === formatted;
 };
 
 // Do not edit below this line
