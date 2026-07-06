aa05858bd76a7f6b56cf14ea4d376feb787bc43a
TimAppleyard <appleyard.timothy@gmail.com>
2026-07-06 18:45:36 -0500

    Completed fibonacci excersice

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(steps) {
+    steps = Number(steps)
+    if(steps < 0){
+        return "OOPS"
+    } else if(steps == 0){
+        return 0;
+    } else if(steps == 1 || steps == 2){
+        return 1;
+    } else{
+        let prev = 1;
+        let prevprev = 1;
+        let fibo = 0;
+        for(let i = 2; i < steps; i++){
+            fibo = prev + prevprev;
+            prevprev = prev;
+            prev = fibo;
+        }
+        return fibo;
+    }
 };
 
 // Do not edit below this line
