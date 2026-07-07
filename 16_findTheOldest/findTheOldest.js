0e9d84bf5f09d9f5ea1e2017d4710afed1c5701b
TimAppleyard <appleyard.timothy@gmail.com>
2026-07-06 19:17:13 -0500

    Completed the findTheOldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let oldest = people.reduce((oldestPerson, person) =>{
+        let age = 0;
+        let ageOldest = 0;
 
+        if (person.yearOfDeath === undefined)
+            age = new Date().getFullYear() - person.yearOfBirth;
+        else
+            age = person.yearOfDeath - person.yearOfBirth;
+
+        if (oldestPerson.yearOfDeath === undefined)
+            ageOldest = new Date().getFullYear() - oldestPerson.yearOfBirth;
+        else
+            ageOldest = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth;
+        
+        if (age > ageOldest)
+            return person;
+        else
+            return oldestPerson;
+    });
+    return oldest;
 };
 
 // Do not edit below this line
